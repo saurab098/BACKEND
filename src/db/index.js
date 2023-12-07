956641057d08a4c84ab1b5ef77aea1ebb1859e09
mani <110892467+saurab098@users.noreply.github.com>
2023-12-07 16:15:52 +0545

    connect to mongodb

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionAny = await mongoose.connect(`${process.env.MONGODB_URI}`);
+    console.log(
+      `Connected MongoDB !! DB HOST: ${connectionAny.connection.host}`
+    );
+  } catch (error) {
+    console.log("Error in connection of MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+// require("dotenv").config({ path: "./.env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB();
+
+/*
+import express from "express";
+const app = express()
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("errror", (error) => {
+      console.log("Erorrr:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is ready in port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error");
+    throw err;
+  }
+});
+
+*/
